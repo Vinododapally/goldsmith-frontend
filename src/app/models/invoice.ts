95ba8347752ea0c07a33b7129108f4e09d76a1a1
Vinod <odapallyvinod@gmail.com>
2023-01-10 21:03:48 +0530

    Added required fields

---
+++ src/app/models/invoice.ts
@@ -0,0 +1,14 @@
+export class Invoice {
+    refId: number;
+    orderNumber: number;
+    showroom: string;
+    itemName: string;
+    grossWeight: number;
+    totalStones: number;
+    netWeight: number;
+    gold92Per: string;
+    gold12Per: string;
+    deliveryDate: Date;
+    makingCharges: number;
+    stoneWeight: number;
+}
