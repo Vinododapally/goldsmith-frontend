573f3897d3aaecaa6e809bd3fba524ab519a8ceb
Vinod <odapallyvinod@gmail.com>
2023-01-18 19:12:11 +0530

    view changes

--- src/app/show-room/showroom-list/showroom-list.component.ts
+++ src/app/show-room/showroom-list/showroom-list.component.ts
@@ -1,10 +1,9 @@
 
-import { Observable } from "rxjs";
 import { Component, OnInit } from "@angular/core";
 import { Router } from '@angular/router';
-import { ShowRoom } from "src/app/models/showRoom";
 import { ShowRoomService } from "src/app/services/showroom.service";
 import { AlertService } from "src/app/services";
+import { PageChangedEvent } from "ngx-bootstrap";
 
 
 @Component({
@@ -13,9 +12,18 @@ import { AlertService } from "src/app/services";
   styleUrls: ["./showroom-list.component.css"]
 })
 export class ShowRoomListComponent implements OnInit {
-  showrooms: Observable<ShowRoom[]>;
   loading = false;
   submitted = false;
+
+  showrooms:any = [];
+  returnedShowRoomArray?: any[];
+  returnedShowRoomArrayCopy?: any[];
+  startCount = 0;
+  endCount = 12;
+  itemsPerPage = [5, 10, 20, 50];
+  selectedItemsPerPage: number = 5;
+  name: any;
+  contactName: any;
 
   constructor(private showRoomService: ShowRoomService,
     private router: Router,private alertService: AlertService) {}
@@ -25,7 +33,11 @@ export class ShowRoomListComponent implements OnInit {
   }
 
   reloadData() {
-    this.showrooms = this.showRoomService.getAll();
+    this.showRoomService.getAll().subscribe((res) => {
+      this.showrooms = res;
+      this.returnedShowRoomArray = this.showrooms.slice(0, this.selectedItemsPerPage);
+      this.returnedShowRoomArrayCopy = JSON.parse(JSON.stringify(this.returnedShowRoomArray));
+    });
   }
 
   deleteShowRoom(id: number) {
@@ -50,5 +62,24 @@ export class ShowRoomListComponent implements OnInit {
   }
   addShowRoom(){
   this.router.navigate(['showroom']);
+  }
+
+  pageChanged(event: PageChangedEvent): void {
+    this.startCount = (event.page - 1) * event.itemsPerPage;
+    this.endCount = event.page * event.itemsPerPage;
+    this.returnedShowRoomArray = this.showrooms.slice(this.startCount, this.endCount);
+  }
+
+  onChangePagination(event) {
+    this.selectedItemsPerPage = event.target.value;
+    this.returnedShowRoomArray = this.showrooms.slice(0, this.selectedItemsPerPage);
+  }
+
+  onFilterChange(event, key) {
+    const filterValue = event.target.value;
+    console.log(filterValue);
+    this.returnedShowRoomArray = this.returnedShowRoomArrayCopy.filter((res) =>
+      res[key].toString().toLowerCase().startsWith(filterValue)
+    );
   }
 }
